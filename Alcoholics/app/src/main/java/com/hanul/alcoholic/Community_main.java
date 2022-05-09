97880c4779f4d99982b76b7b06897217adfe1f4d
jha2ee <jha2ee99@gmail.com>
2022-05-09 20:58:40 +0900

    Connect fragment between community
    메인 커뮤니티 화면에서 listView 아이템 클릭할 때 fragment 이동 기능 추가
    bundle 사용하여 화면 간 데이터 이동 - 나중에 DB에서 값 불러오는 걸로 변경할지 확인 필요

--- Alcoholics/app/src/main/java/com/hanul/alcoholic/Community_main.java
+++ Alcoholics/app/src/main/java/com/hanul/alcoholic/Community_main.java
@@ -3,23 +3,102 @@ package com.hanul.alcoholic;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.List;
+
+/* 리스트 뷰에 들어갈 내용 입력받기 위해 클래스 만들어둠 */
+class Post {
+    private String nickname;
+    private String content;
+    private int comments;
+    private String date;
+
+    Post() {}
+    Post(String nickname, String content, int comments, String date) {
+        this.nickname = nickname;
+        this.content = content;
+        this.comments = comments;
+        this.date = date;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+    public String getContent() {
+        return content;
+    }
+    public int getComments() {
+        return comments;
+    }
+    public String getDate() {
+        return date;
+    }
+
+}
+/* 들어가야 할 내용
+* 1. Firebase에서 content/nickname/timeline/comment 갯수 불러와서 setText()
+* 2. 선택한 listView 항목 - Community_post로 이동해서 fragment 띄우기
+*
+* */
 public class Community_main extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_community_board, container, false);
+        //ListView 아이템에 표시될 사용자 데이터 정의
+        //***** listItems에 들어갈 내용 : firebase에서 데이터 가져오기 (위에 Post 클래스 사용?)
+        String[] listItems = {"sample one", "sample two", "sample three"};
+
+
+        ListView listView = (ListView)view.findViewById(R.id.main_list);
+        //데이터 입력받을 adapter 생성
+        //fragment에서 this사용 안됨 -> Activity의 참조 획득이 가능한 getActivity() 함수 사용
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(listViewAdapter);
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_community_board, container, false);
+
+        //listView 클릭했을 때 - 클릭한 게시글로 fragment 이동
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Bundle data = new Bundle();
+                //Post 클래스의 내용 활용하기 sample -- **** firebase 값 가져오기
+                //Bundle 통해 값 전달하기
+                Post post = new Post("nick", "sample", 0, "20220509");
+                data.putString("nickname", post.getNickname());
+                data.putString("post", post.getContent());
+                //data.putInt("comments", post.getComments());
+                data.putString("date", post.getDate());
+
+                //값을 넘길 fragment 선언하고 전달
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Community_post chosen_post = new Community_post();
+                chosen_post.setArguments(data);
+                transaction.replace(R.id.postContext, chosen_post);
+                transaction.commit();
+
+            }
+        });
+
+
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
     }
+
 }
--- Alcoholics/app/src/main/java/com/hanul/alcoholic/Community_post.java
+++ Alcoholics/app/src/main/java/com/hanul/alcoholic/Community_post.java
@@ -4,17 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+/* 바꿔야할 부분??
+* 현재 bundle로 메인 community에서 값 받아오는데- firebase에서 값 가져올건지?
+*
+* */
 public class Community_post extends Fragment {
+    private View view;
+
+    private TextView nickname;
+    private TextView timeline;
+    private TextView post;
+    private TextView reply_list;
+
+    private String get_nickname;
+    private String get_timeline;
+    private String get_post;
+    private String get_reply_list;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_community_post, container, false);
+        view = inflater.inflate(R.layout.fragment_community_post, container, false);
+        if (getArguments() != null) {
+            //bundle에 값이 있다면 각 key값 불러서 setText()
+            get_nickname = getArguments().getString("nickname");
+            nickname = view.findViewById(R.id.nickname);
+            nickname.setText(get_nickname);
+
+            get_timeline = getArguments().getString("date");
+            timeline = view.findViewById(R.id.timeline);
+            timeline.setText(get_timeline);
+
+            get_post = getArguments().getString("post");
+            post = view.findViewById(R.id.post);
+            post.setText(get_post);
+
+            //replylist - firebase에서 값 가져오기
+            get_reply_list = "";
+            reply_list = view.findViewById(R.id.reply_list);
+            reply_list.setText(get_reply_list);
+
+        }
+
+        return view;
     }
 
     @Override
