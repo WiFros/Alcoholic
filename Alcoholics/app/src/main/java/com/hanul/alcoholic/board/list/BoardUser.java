3453990e5bc641c01f4ffb27ecdceb75ed81e8ff
WiFros <bjy123bjy@gmail.com>
2022-05-19 20:22:19 +0900

    firebase, recycleview
    파이어베이스에서 데이터를 불러와 화면에 띄우기

---
+++ Alcoholics/app/src/main/java/com/hanul/alcoholic/board/list/BoardUser.java
@@ -0,0 +1,44 @@
+package com.hanul.alcoholic.board.list;
+
+import com.hanul.alcoholic.Board;
+
+public class BoardUser {
+    private String profile;
+    private  String id;
+    private int pw;
+    private String userName;
+
+    public BoardUser(){}
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPw() {
+        return pw;
+    }
+
+    public void setPw(int pw) {
+        this.pw = pw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
